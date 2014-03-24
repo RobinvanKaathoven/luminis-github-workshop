f1f13aaa1083053d9778bd25eaaf5d6ef6ee4b28
Robin van Kaathoven <robin.vankaathoven@luminis.eu>
2014-03-24 13:53:50 +0100

    result for grails create-domain-class com.luminis.university.Participant

---
+++ test/unit/com/luminis/university/eventmanager/ParticipantTests.groovy
@@ -0,0 +1,17 @@
+package com.luminis.university.eventmanager
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Participant)
+class ParticipantTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}

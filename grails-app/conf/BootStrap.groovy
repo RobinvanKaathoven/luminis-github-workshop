2fe9c1257b0e47db967943f66cf979d316801ab9
Robin van Kaathoven <robin.vankaathoven@luminis.eu>
2014-03-24 15:01:37 +0100

    Extended bootstrapping

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,3 +1,4 @@
+import com.luminis.university.eventmanager.Entry
 import com.luminis.university.eventmanager.Event
 import com.luminis.university.eventmanager.Participant
 
@@ -6,13 +7,18 @@ class BootStrap {
     def init = { servletContext ->
 
         //Add some events
-        new Event(name: "Luminis University Grails 101").save()
-        new Event(name: "Luminis University Grails 102").save()
-        new Event(name: "Koningsdag").save()
+        def grails101 = new Event(name: "Luminis University Grails 101").save()
+        def grails102 = new Event(name: "Luminis University Grails 102").save()
+        def koningsdag = new Event(name: "Koningsdag").save()
 
         //Add some participants
-        new Participant(firstName: "Robin", lastName: "van Kaathoven").save()
-        new Participant(firstName: "Frank", lastName: "Wolferink").save()
+        def robin = new Participant(firstName: "Robin", lastName: "van Kaathoven").save()
+        def frank = new Participant(firstName: "Frank", lastName: "Wolferink").save()
+
+        //Add some entries
+        def robinGrails101Entry = new Entry(participant: robin, event: grails101).save()
+        def robinGrails102Entry = new Entry(participant: robin, event: grails102).save()
+        def frankGrails101Entry = new Entry(participant: frank, event: grails101).save()
 
     }
     def destroy = {

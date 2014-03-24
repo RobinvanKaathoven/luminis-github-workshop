b4fd6d8e25b0e23585be9b80aacdd5134971cc37
Robin van Kaathoven <robin.vankaathoven@luminis.eu>
2014-03-24 14:38:53 +0100

    result of grails create-domain-class com.luminis.university.Entry and added a Participant and Event variable to the domain object

---
+++ grails-app/domain/com/luminis/university/eventmanager/Entry.groovy
@@ -0,0 +1,13 @@
+package com.luminis.university.eventmanager
+
+class Entry {
+
+    static belongsTo = [
+            event: Event,
+            participant: Participant
+    ]
+
+    static constraints = {
+    }
+
+}
---
+++ test/unit/com/luminis/university/eventmanager/EntryTests.groovy
@@ -0,0 +1,17 @@
+package com.luminis.university.eventmanager
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Entry)
+class EntryTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}

dd31e32a58e2979a993bac7e678c98f8c26f001a
Robin van Kaathoven <robin.vankaathoven@luminis.eu>
2014-03-25 11:47:35 +0100

    Added two dates to indicate the start and end of an event

--- grails-app/domain/com/luminis/university/eventmanager/Event.groovy
+++ grails-app/domain/com/luminis/university/eventmanager/Event.groovy
@@ -3,8 +3,17 @@ package com.luminis.university.eventmanager
 class Event {
 
     static constraints = {
+        name blank: false, minSize: 5
+        eventStart nullable: true
+        eventEnd nullable: true
     }
 
+    static mapping = {
+        sort eventStart: "asc"
+    }
+
+    Date eventStart
+    Date eventEnd
     Date dateCreated
     String name
 
